643dff3b4abfb3f9029c11add391b275cd92b9f2
wujiehong <544268828@qq.com>
2019-07-31 01:40:08 +0800

    头底部，首页完成，搜索栏有小高亮bug

---
+++ components/header.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="view">
+    <div class="header">
+      <el-row type="flex" justify="space-between">
+        <div class="logo">
+          <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+        </div>
+        <div class="nav">
+          <nuxt-link to="/">首页</nuxt-link>
+          <nuxt-link to="/post">旅游攻略</nuxt-link>
+          <nuxt-link to="/hotel">酒店</nuxt-link>
+          <nuxt-link to="/air">国内机票</nuxt-link>
+        </div>
+        <div class="login">
+          <img src alt />
+          <el-dropdown>
+            <div class="menus">
+              <img src="http://157.122.54.189:9095/assets/images/avatar.jpg" alt />
+              <el-button>
+                登录/注册
+                <i class="el-icon-arrow-down el-icon--right"></i>
+              </el-button>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>个人中心</el-dropdown-item>
+                <el-dropdown-item>退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </div>
+          </el-dropdown>
+        </div>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<style lang='less' scoped>
+.view {
+
+}
+ul li ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  &:hover {
+    color: #409eff;
+  }
+}
+.header {
+  box-sizing: border-box;
+  border-bottom: 1px solid #ccc;
+  box-shadow: 0 1px 3px #eee;
+  font-size: 18px;
+  height: 60px;
+  width: 1000px;
+  margin: 0 auto;
+  line-height: 60px;
+  .logo {
+    img {
+      width: 156px;
+      height: 43px;
+      display: block;
+      margin-top: 8px;
+    }
+  }
+  .login {
+    margin-top:-2px;
+    img {
+      border-radius: 50%;
+      width: 36px;
+      height: 36px;
+      vertical-align: middle;
+      border: 3px solid #fff;
+      &:hover {
+        border: 3px solid #409eff;
+      }
+    }
+  }
+  .nav {
+    flex: 1;
+    display: flex;
+    padding-left: 30px;
+    a {
+      display: block;
+      height: 60px;
+      box-sizing: border-box;
+      padding: 0 20px;
+      line-height: 60px;
+      //  border-bottom: 5px solid #fff;
+      &:hover {
+        border-bottom: 5px solid #409eff;
+      }
+    }
+  }
+}
+.nuxt-link-exact-active {
+  background: #409eff;
+  color: #fff;
+  &:hover {
+    color: #fff;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="index">
+    <!-- 轮播图 -->
+    <el-carousel trigger="click" height="700px">
+      <el-carousel-item v-for="(item,index) in banners" :key="index">
+        <div
+          class="banner"
+          :style="`background:url(${$axios.defaults.baseURL+item.url}) center center no-repeat`"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
+    <!-- 标签页 -->
+    <el-tabs type="border-card">
+      <el-tab-pane v-for="(item,index) in search" :key="index">
+        <span slot="label" @click="deal(index)">
+          <i :class="item.icon"></i>{{item.text}}
+        </span>
+        </el-tab-pane>
+        <!-- 输入搜索框 -->
+        <div style="margin-top: 2px;">
+          <el-input :placeholder="search[current].placeholder" v-model="searchWord" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </div>
+      
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      banners: [],
+      search: [
+        {
+          icon: "el-icon-s-flag",
+          text: "攻略",
+          placeholder: "请输入城市"
+        },
+        {
+          icon: "el-icon-office-building",
+          text: "酒店",
+          placeholder: "请输入城市搜索酒店"
+        },
+        {
+          icon: "el-icon-position",
+          text: "机票",
+          placeholder: "请输入城市搜索机票"
+        }
+      ],
+      searchWord: "",
+      current:0,
+    };
+  },
+  methods:{
+    deal(index){
+      console.log(index);
+      this.current=index;
+      if(index===2){
+        this.$router.push("/air")
+      }
+    }
+  },
+  mounted() {
+    console.log(this.$axios.defaults.baseURL);
+    this.$axios({
+      url: "/scenics/banners"
+    })
+      .then(res => {
+        console.log(res);
+        this.banners = res.data.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.banner {
+  width: 100%;
+  height: 100%;
+}
+/deep/ .el-tabs__item{
+  font-size: 18px;
+  color:#fff !important;
+  padding: 0,10px;
+  background: rgba(0,0,0,.5);
+  i{
+    font-size: 24px;
+    margin-right: 5px
+  }
+}
+/deep/ .el-tabs{
+  z-index:9;
+  width: 550px;
+  float: left;
+  margin-top: -400px;
+  margin-left: 400px;
+  margin-bottom:-2px;
+  background: rgba(0,0,0,.5);
+}
+/deep/ .el-tabs__item{
+   border-radius: 5px;
+}
+/deep/ .is-active{
+  background: #fff;
+  color:#333 !important;
+}
+/deep/ .el-carousel__indicator--horizontal{
+  padding: 0 !important;
+}
+/deep/ .el-tabs__header{
+    background: rgba(0,0,0,.5);
+  width: 318px;
+  border-radius:5px 
+}
+/deep/ .el-tabs__content{
+  z-index: 9;
+  padding: 0;
+  margin-top: -5px;
+}
+</style>
